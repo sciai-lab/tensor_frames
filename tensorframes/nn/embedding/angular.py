068666a4b73f7a37a01098c927cf638f6cce6ffd
PeteLipp <73332106+PeteLipp@users.noreply.github.com>
2024-07-17 11:50:24 +0200

    added gaussian on sphere to radial embeddings

--- tensorframes/nn/embedding/angular.py
+++ tensorframes/nn/embedding/angular.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Union
 
 import e3nn.o3 as o3
+import numpy as np
 import torch
 from torch import Tensor
 
@@ -79,7 +80,7 @@ class TrivialAngularEmbedding(AngularEmbedding):
             return edge_vec
 
 
-class SphericalHarmonicsEmbedding(torch.nn.Module):
+class SphericalHarmonicsEmbedding(AngularEmbedding):
     """Spherical Harmonics Embedding module."""
 
     def __init__(self, lmax: int = 2, normalization: str = "norm"):
@@ -107,3 +108,83 @@ class SphericalHarmonicsEmbedding(torch.nn.Module):
         return o3.spherical_harmonics(
             self.irreps_sh, edge_vec, normalize=True, normalization=self.normalization
         )[..., 1:]
+
+
+def fibonacci_sphere(samples=1000):
+    """Generate points on a sphere using the Fibonacci sphere algorithm.
+
+    Args:
+        samples (int): The number of points to generate on the sphere. Default is 1000.
+
+    Returns:
+        list: A list of tuples representing the points on the sphere. Each tuple contains the x, y, and z coordinates.
+    """
+    points = []
+    phi = np.pi * (np.sqrt(5.0) - 1.0)  # golden angle in radians
+
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = np.sqrt(1 - y * y)  # radius at y
+
+        theta = phi * i  # golden angle increment
+
+        x = np.cos(theta) * radius
+        z = np.sin(theta) * radius
+
+        points.append((x, y, z))
+
+    return points
+
+
+class GaussianOnSphereEmbedding(AngularEmbedding):
+    """A class representing a Gaussian embedding on a sphere.
+
+    Args:
+        num_angular (int): The number of equi-spaced points on the sphere.
+        normalized (bool, optional): Whether to normalize the gaussians. Defaults to True.
+        is_learnable (bool, optional): Whether the gaussian widths are learnable parameters. Defaults to True.
+    """
+
+    def __init__(self, num_angular: int, normalized: bool = True, is_learnable: bool = True):
+        super().__init__()
+        self.normalized = normalized
+
+        # get num_angular equi spaced points on the sphere:
+        points = torch.tensor(fibonacci_sphere(num_angular))
+
+        # calculate mean of the difference of the points
+        mean = torch.mean(torch.norm(points[1:] - points[:-1]), dim=0)
+        widths = mean.repeat(num_angular).view(1, num_angular)
+
+        # initialize the gaussian widths to be the mean of the difference of the points
+        if is_learnable:
+            self.centers = torch.nn.Parameter(data=points)
+            self.widths = torch.nn.Parameter(data=widths)
+        else:
+            self.register_buffer("centers", points)
+            self.register_buffer("widths", widths)
+
+        self.out_dim = num_angular
+
+    def compute_embedding(self, edge_vec: Tensor) -> Tensor:
+        """Compute the Gaussian embedding for the given edge vector.
+
+        Args:
+            edge_vec (Tensor): The input edge vector.
+
+        Returns:
+            Tensor: The computed Gaussian embedding.
+        """
+        edge_vec = edge_vec / torch.clamp(
+            torch.linalg.norm(edge_vec, dim=-1, keepdim=True), min=1e-9
+        )
+        diff_norm = torch.linalg.norm(edge_vec.unsqueeze(1) - self.centers, dim=-1)
+
+        # calculate the gaussian embedding
+        exp = torch.exp(-0.5 * (diff_norm / self.widths) ** 2).to(edge_vec.dtype)
+
+        # normalize the gaussians
+        if self.normalized:
+            exp = exp / torch.sqrt(self.widths * torch.pi).to(edge_vec.dtype)
+
+        return exp
