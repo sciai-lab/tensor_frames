2d01bf626975a47ec27c8a16343fbbed33d6b6f8
gerhagj <gerrit.gerhartz@gmail.com>
2024-07-30 11:53:28 +0200

    added imports in init

--- tensorframes/lframes/__init__.py
+++ tensorframes/lframes/__init__.py
@@ -1,1 +1,9 @@
+from classical_lframes import (
+    IdentityLFrames,
+    RandomGlobalLFrames,
+    RandomLFrames,
+    ThreeNNLFrames,
+)
+from learning_lframes import LearnedGramSchmidtLFrames
+
 from tensorframes.lframes.lframes import ChangeOfLFrames, LFrames
