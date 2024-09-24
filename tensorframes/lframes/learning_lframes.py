52c397fe5fe032d6c19490221757e6f6f221ddce
PeteLipp <73332106+PeteLipp@users.noreply.github.com>
2024-09-24 17:10:26 +0200

    added restricted equivariance to rotations around the gravitational axis

--- tensorframes/lframes/learning_lframes.py
+++ tensorframes/lframes/learning_lframes.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import numpy as np
 import torch
 from torch import Tensor
 from torch.nn import Module
@@ -31,6 +32,8 @@ class LearnedGramSchmidtLFrames(MessagePassing, LFramesPredictionModule):
         concat_receiver: bool = True,
         exceptional_choice: str = "random",
         anchor_z_axis: bool = True,
+        fix_gravitational_axis: bool = False,
+        gravitational_axis_index: int = 1,
         envelope: Union[torch.nn.Module, None] = EnvelopePoly(5),
         **mlp_kwargs: dict,
     ) -> None:
@@ -46,6 +49,8 @@ class LearnedGramSchmidtLFrames(MessagePassing, LFramesPredictionModule):
             concat_receiver (bool, optional): Whether to concatenate the receiver input to the mlp input. Defaults to True.
             exceptional_choice (str, optional): The exceptional choice, which is used by gram schmidt. Defaults to "random".
             anchor_z_axis (bool, optional): Whether to anchor the z-axis. Defaults to False.
+            fix_gravitational_axis (bool, optional): Whether to fix the graviational axis. Defaults to False.
+            gravitational_axis_index (int, optional): The index of the gravitational axis. Defaults to 1.
             envelope (Union[torch.nn.Module, None], optional): The envelope module. Defaults to EnvelopePoly(5).
             **mlp_kwargs (dict): Additional keyword arguments for the MLP.
         """
@@ -68,6 +73,22 @@ class LearnedGramSchmidtLFrames(MessagePassing, LFramesPredictionModule):
                 self.predict_o3
             ), f"anchor_z_axis only works with predict_o3, predict_o3 = {predict_o3}"
             self.num_pred_vecs -= 1
+
+        if fix_gravitational_axis:
+            graviational_axis = torch.zeros(3)
+            graviational_axis[gravitational_axis_index] = 1.0
+            self.register_buffer("graviational_axis", graviational_axis)
+            self.num_pred_vecs -= 1
+
+            # find the even permutation where index_order[gravitational_axis_index] is 0:
+            index_order = [0, 1, 2]
+            for i in range(3):
+                current_index_order = np.roll(index_order, i)
+                if current_index_order[gravitational_axis_index] == 0:
+                    self.index_order = current_index_order.tolist()
+        else:
+            self.graviational_axis = None
+            self.index_order = None
 
         self.hidden_channels.append(self.num_pred_vecs)
 
@@ -135,17 +156,34 @@ class LearnedGramSchmidtLFrames(MessagePassing, LFramesPredictionModule):
             vecs = vecs.reshape(-1, self.num_pred_vecs, 3)
 
         if self.predict_o3:
+            if self.graviational_axis is None:
+                vec1 = vecs[:, 0, :]
+                vec2 = vecs[:, 1, :]
+                vec3 = vecs[:, 2, :]
+            else:
+                vec1 = self.graviational_axis[None, :].repeat(vecs.shape[0], 1)
+                vec2 = vecs[:, 0, :]
+                vec3 = vecs[:, 1, :]
+
             local_frames = gram_schmidt(
-                vecs[:, 0, :],
-                vecs[:, 1, :],
-                vecs[:, 2, :],
+                vec1,
+                vec2,
+                vec3,
                 exceptional_choice=self.exceptional_choice,
             )
         else:
-            local_frames = gram_schmidt(
-                vecs[:, 0, :], vecs[:, 1, :], exceptional_choice=self.exceptional_choice
-            )
+            if self.graviational_axis is None:
+                vec1 = vecs[:, 0, :]
+                vec2 = vecs[:, 1, :]
+            else:
+                vec1 = self.graviational_axis[None, :].repeat(vecs.shape[0], 1)
+                vec2 = vecs[:, 0, :]
 
+            local_frames = gram_schmidt(vec1, vec2, exceptional_choice=self.exceptional_choice)
+
+        # permute the axes to be at the correct index position:
+        if self.index_order is not None:
+            local_frames = local_frames[:, self.index_order]
         return LFrames(local_frames)
 
     def message(
